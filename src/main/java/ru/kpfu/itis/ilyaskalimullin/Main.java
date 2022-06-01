96f1fcd17de77d66025098cab783c16f3a8d6f82
Ilyas <ilyaskalimullinn@yandex.ru>
2022-06-01 18:39:31 +0300

    fixed ScheduleParser; constructor still automatically writes in the file schedule.xml (can be changed); split lessons (e.g. english) don't have group number; added map getter from xml file

--- src/main/java/ru/kpfu/itis/ilyaskalimullin/Main.java
+++ src/main/java/ru/kpfu/itis/ilyaskalimullin/Main.java
@@ -1,9 +1,15 @@
 package ru.kpfu.itis.ilyaskalimullin;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         try {
             ScheduleParser parser = new ScheduleParser(1);
+
+            Map<String, String> map = parser.getScheduleMapFromXML();
+
+            System.out.println(map);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
--- src/main/java/ru/kpfu/itis/ilyaskalimullin/ScheduleParser.java
+++ src/main/java/ru/kpfu/itis/ilyaskalimullin/ScheduleParser.java
@@ -3,12 +3,12 @@ package ru.kpfu.itis.ilyaskalimullin;
 import com.google.gson.Gson;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,7 +25,11 @@ public class ScheduleParser {
         setWeekDay(weekDay);
         url = new URL(scriptUrlString + "?weekDay=" + weekDay);
 
-        writeSchedule(parseSchedule());
+        saveRootSchedule(
+                parseRootFromJSON(
+                        getJSONFromSheet()
+                )
+        );
 
     }
 
@@ -33,7 +37,7 @@ public class ScheduleParser {
         this(DEFAULT_PATH_STRING, DEFAULT_SCRIPT_URL_STRING, weekDay);
     }
 
-    public Root parseSchedule() throws IOException {
+    public String getJSONFromSheet() throws IOException {
         String json;
 
         try (BufferedReader in = new BufferedReader(
@@ -42,15 +46,18 @@ public class ScheduleParser {
                 )
         )) {
             json = in.readLine();
+            return json;
         } catch (IOException ex) {
             throw new IOException(ex);
         }
+    }
 
+    public Root parseRootFromJSON(String json) throws IOException {
         Gson gson = new Gson();
         return gson.fromJson(json, Root.class);
     }
 
-    public void writeSchedule(Root root) throws IOException {
+    public void saveRootSchedule(Root root) throws IOException {
         BufferedWriter out = new BufferedWriter(
                 new OutputStreamWriter(
                         new FileOutputStream(path.toFile())
@@ -60,28 +67,22 @@ public class ScheduleParser {
         out.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
         out.write("<resources>");
 
-        Pattern pattern = Pattern.compile("([^,]+),([^,]*),([^,]*)");
-        Matcher matcher;
 
-        for (int i = 1; i <= root.schedule.size(); i++) {
-            for (int j = 1; j <= root.schedule.get(i - 1).size(); j++) {
 
-                String currentCell = root.schedule.get(i - 1).get(j - 1);
+        for (int lessonNumber = 1; lessonNumber <= root.schedule.size(); lessonNumber++) {
+            for (int groupNumber = 1; groupNumber <= root.schedule.get(lessonNumber - 1).size(); groupNumber++) {
+
+                String currentCell = root.schedule.get(lessonNumber - 1).get(groupNumber - 1);
                 String[] currentCellSplit = currentCell.split(";");
 
                 if (currentCellSplit.length == 1) {
                     String lesson = currentCellSplit[0];
-                    matcher = pattern.matcher(lesson);
-                    if (!matcher.find()) continue;
-                    if (matcher.group(3).equals("")) continue;
-                    out.write("<string name = \"cab" + matcher.group(3) + "\">" + matcher.group(1) + " " + matcher.group(2) + " + " + getGroupNumber(j) + "</string>");
-                }
+                    saveLesson(lesson, lessonNumber, groupNumber, false, out);
+                } else {
 
-                for (String lesson : currentCellSplit) {
-                    matcher = pattern.matcher(lesson);
-                    if (!matcher.find()) continue;
-                    if (matcher.group(3).equals("")) continue;
-                    out.write("<string name = \"cab" + matcher.group(3) + "\">" + matcher.group(1) + " " + matcher.group(2) + " + </string>");
+                    for (String lesson : currentCellSplit) {
+                        saveLesson(lesson, lessonNumber, groupNumber, true, out);
+                    }
                 }
             }
         }
@@ -96,7 +97,57 @@ public class ScheduleParser {
         this.weekDay = weekDay;
     }
 
-    public String getGroupNumber(int number) {
+    public boolean saveLesson(String lesson, int lessonNumber, int groupNumber, boolean isSplitLesson, Writer out) throws IOException {
+        Pattern pattern = Pattern.compile("([^,]+),([^,]*),([^,]*)");
+        Matcher matcher = pattern.matcher(lesson);
+        if (!matcher.find()) return false;
+
+        String lessonName = matcher.group(1);
+        String lessonTeacher = matcher.group(2);
+        String lessonCab = matcher.group(3);
+
+        String group;
+        if (isSplitLesson) {
+            group = "";
+        } else {
+            group = " + " + getGroup(groupNumber);
+        }
+
+        if (matcher.group(3).equals("")) return false;
+        //out.write("<string name = \"cab" + matcher.group(3) + " " + lessonNumber + "\">" + matcher.group(1) + " " + matcher.group(2) + " + " + getGroupNumber(groupNumber) + "</string>");
+        out.write(String.format("<string name = \"%s %d\">%s %s%s</string>", lessonCab, lessonNumber, lessonName, lessonTeacher, group));
+        return true;
+    }
+
+    public Map<String, String> getScheduleMapFromXML() throws IOException {
+        Map<String, String> map = new HashMap<>();
+
+        String xmlString;
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(path.toFile())
+                )
+        )) {
+            xmlString = in.lines().reduce((s1, s2) -> s1 + s2).get();
+        } catch (IOException ex) {
+            throw new IOException("Could not read schedule xml file", ex);
+        }
+
+        Pattern pattern = Pattern.compile("<string name = \"([0-9 ]+)\">([^<]+)<\\/string>");
+        Matcher matcher = pattern.matcher(xmlString);
+
+        while (matcher.find()) {
+            String key = matcher.group(1);
+            String value = matcher.group(2);
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    public String getGroup(int number) {
         String numberString = "11-%03d";
 
         //52 groups
